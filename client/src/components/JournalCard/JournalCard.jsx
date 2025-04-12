69a40975683119d23993bc979f1f6236952303d3
Blair Crumbly <blaircrumbly@gmail.com>
2025-04-12 00:51:12 -0400

    moved date of journal to bottom of journal card

--- client/src/components/JournalCard/JournalCard.jsx
+++ client/src/components/JournalCard/JournalCard.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import './JournalCard.css';
 
+
 const JournalCard = ({ journal, onClick }) => {
-    return (
-      <div
-        className="journal-card"
-        style={{ backgroundColor: journal.color }}
-        onClick={() => onClick(journal.id)}
-      >
-        <div className="journal-card-content">
-          <h3>{journal.title} {journal.year}</h3>
-        </div>
+  return (
+    <div
+      className="journal-card"
+      style={{ backgroundColor: journal.color }}
+      onClick={() => onClick(journal.id)}
+    >
+      <div className="journal-card-content">
+        <h3>{journal.title}</h3>
+        <div className="journal-year">{journal.year}</div>
       </div>
-    );
-  };
-  
-  export default JournalCard;
+    </div>
+  );
+};
+
+export default JournalCard;
